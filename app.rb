e341b2ef0ab299bdac99173e0f62620b22706b71
Ben <henriquez.ben@gmail.com>
2016-10-14 14:14:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,42 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    reverse = params[:name].split("").reverse.join
+    "#{reverse}"
+  end
+
+  get '/square/:number' do
+    "#{params[:number].to_i * params[:number].to_i}"
+  end
+
+  get '/say/:number/:phrase' do
+    num = (params[:number]).to_i
+    phrase_space = params[:phrase].split("%20").join(" ")
+    string = ""
+    times = for i in 0..num
+       string = string + params[:phrase] + "\n"
+     end
+     "#{string}"
+    # binding.pry
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    if params[:operation] == "add"
+      "#{params[:number1].to_i + params[:number2].to_i}"
+    elsif params[:operation] == "subtract"
+      "#{params[:number1].to_i - params[:number2].to_i}"
+    elsif params[:operation] == "multiply"
+      "#{params[:number1].to_i * params[:number2].to_i}"
+    elsif params[:operation] == "divide"
+      "#{params[:number1].to_i / params[:number2].to_i}"
+  end
+end
+
 end
